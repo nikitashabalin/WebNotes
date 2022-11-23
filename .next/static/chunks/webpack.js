0164c873decf786cecaf9b3ab5227ee813b42af9
Nikita Shabalin <folgergames@gmail.com>
2022-11-23 23:49:57 +0300

    version v2

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "ba5ca1c564a84ede"; }
+/******/ 		__webpack_require__.h = function() { return "44f837b4646e1deb"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,38 +1,46 @@
+import Link from "next/link";
 import { useEffect, useState } from "react";
 
 interface NoteInter {
   name: string;
   desc: string;
+  type: number;
 }
 
 const Home = () => {
-  const [newNote, setNewNote] = useState<string>("");
+  const [filterButton, setFilterButton] = useState<number>(0);
+
   const [noteArray, setNoteArray] = useState<NoteInter[]>([]);
+  const [newNote, setNewNote] = useState<string>("");
 
   const [openNote, setOpenNote] = useState<number>(-1);
   const [desc, setDesk] = useState<string>("");
 
-  const [rel, reload] = useState<boolean>(false);
+  const [rel, setRel] = useState<boolean>(false);
 
   const addButton = () => {
-    noteArray.push({ name: newNote, desc: "" });
-    setNoteArray(noteArray);
+    const newArray = [
+      { name: newNote, desc: "", type: filterButton === -1 ? 0 : filterButton },
+      ...noteArray,
+    ];
+
+    setNoteArray(newArray);
     setNewNote("");
-    localStorage.setItem("array", JSON.stringify(noteArray));
-    reload(!rel);
+
+    localStorage.setItem("array", JSON.stringify(newArray));
   };
 
-  const deleteButton = (index: number) => {
-    noteArray.splice(index, 1);
-    setNoteArray(noteArray);
-    localStorage.setItem("array", JSON.stringify(noteArray));
-    reload(!rel);
+  const deleteButton = (indexNum: number) => {
+    const newArray = noteArray.filter((el, index) => index !== indexNum);
+
+    setNoteArray(newArray);
+
+    localStorage.setItem("array", JSON.stringify(newArray));
   };
 
   const openNoteButton = (index: number) => {
     setOpenNote(index);
     setDesk(noteArray[index].desc);
-    reload(!rel);
   };
 
   const addDesc = (index: number) => {
@@ -41,7 +49,6 @@ const Home = () => {
     setOpenNote(-1);
     setDesk("");
     localStorage.setItem("array", JSON.stringify(noteArray));
-    reload(!rel);
   };
 
   useEffect(() => {
@@ -50,59 +57,108 @@ const Home = () => {
     }
   }, []);
 
+  const changeFilter = (index: number) => {
+    setFilterButton(index);
+    setRel(!rel);
+  };
+
+  const filterType = (index: number) => {
+    return { [-1]: "Все заметки", [0]: "Важно", [1]: "Запланировано", [2]: "Напомоинание" }[index];
+  };
+
   return (
     <>
-    <div className="main">
-      <div className="menu">
-        <h1 className="title">MyNote</h1>
-        <div className="menu__nav">
-          <div className="menu__nav-title"><a href="">Все заметки</a></div>
-          <div className="menu__nav-title"><a href="">Важно</a></div>
-          <div className="menu__nav-title"><a href="">Запланировано</a></div>
-          <div className="menu__nav-title"><a href="">Напоминания</a></div>
+      <div className="main">
+        <div className="menu">
+          <h1 className="title">MyNote</h1>
+          <div className="menu__nav">
+            <div className="menu__nav-title">
+              <div onClick={() => changeFilter(-1)}>Все заметки</div>
+            </div>
+            <div className="menu__nav-title">
+              <div onClick={() => changeFilter(0)}>Важно</div>
+            </div>
+            <div className="menu__nav-title">
+              <div onClick={() => changeFilter(1)}>Запланировано</div>
+            </div>
+            <div className="menu__nav-title">
+              <div onClick={() => changeFilter(2)}>Напоминания</div>
+            </div>
+          </div>
         </div>
-      </div>
-      <div className="todo">
-        <div className="todo__content">
-            <h1 className="todo__title">Все заметки</h1>
+        <div className="todo">
+          <div className="todo__content">
+            <h1 className="todo__title">{filterType(filterButton)}</h1>
             <div className="todo__new">
-                <input type="text" placeholder="Новая заметка" value={newNote} onChange={(e) => setNewNote(e.target.value)}/>
-                <div className="todo__add" onClick={addButton}>+</div>
+              <input
+                type="text"
+                placeholder="Новая заметка"
+                value={newNote}
+                onChange={(e) => setNewNote(e.target.value)}
+              />
+              <div className="todo__add" onClick={addButton}>
+                +
+              </div>
             </div>
             <div className="todo__list">
-              {noteArray.map((el, index) => (
-                <div className="todo__task" key={index} >
-                  <div className="todo__task-container">
-                    <label className="todo_checkbox">
-                          <input type="checkbox"/>
+              {noteArray
+                .filter((el) =>
+                  filterButton === -1 ? true : el.type === filterButton,
+                )
+                .map((el, index) => (
+                  <div
+                    className="todo__task"
+                    key={index}
+                    onClick={() => openNoteButton(index)}
+                  >
+                    <div
+                      style={{
+                        display: "flex",
+                        justifyContent: "space-between",
+                        width: "100%",
+                        height: "36px",
+                      }}
+                    >
+                      <div className="todo__task-container">
+                        <label className="todo_checkbox">
+                          <input type="checkbox" />
                           <div></div>
-                    </label>
-                    
-                      <div className="todo__task-text" onClick={() => openNoteButton(index)}>{el.name}</div>
-                      
-                      
-
-                  </div>
-                  
-                    {openNote === index && (
-                    <div className="todo__task-write">
-                      <textarea className="todo__task-note"
-                        value={desc}
-                        onChange={(e) => setDesk(e.target.value)}
-                      />
-                      <button onClick={() => addDesc(index)}>Скрыть</button>
+                        </label>
+                        <div className="todo__task-text">{el.name}</div>
+                        <div
+                          className="todo__task-text"
+                          style={{
+                            marginLeft: "20px",
+                            opacity: 0.3,
+                          }}
+                        >
+                          {filterType(el.type)}
+                        </div>
+                      </div>
+                      <div
+                        className="todo__task-del"
+                        onClick={() => deleteButton(index)}
+                      >
+                        -
+                      </div>
                     </div>
-                  )}
-                  <div className="todo__task-del" onClick={() => deleteButton(index)}>-</div> 
-                </div>
-              ))}
+
+                    {openNote === index && (
+                      <div className="todo__task-write">
+                        <textarea
+                          className="todo__task-note"
+                          value={desc}
+                          onChange={(e) => setDesk(e.target.value)}
+                        />
+                        <button onClick={() => addDesc(index)}>Скрыть</button>
+                      </div>
+                    )}
+                  </div>
+                ))}
             </div>
+          </div>
         </div>
-        
       </div>
-      
-    </div>
-   
     </>
   );
 };
